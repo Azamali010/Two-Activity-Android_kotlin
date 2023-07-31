6b34c7607aaa0060b4b6342aa1399d8f61d122b1
Azamali010 <azamali07711@gmail.com>
2023-07-31 23:22:37 +0530

    five

--- app/src/main/java/com/example/twoactivity/MainActivity2.kt
+++ app/src/main/java/com/example/twoactivity/MainActivity2.kt
@@ -1,13 +1,17 @@
 package com.example.twoactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var aUserName : TextView
     private lateinit var bUserPassword :TextView
+   // private lateinit var btn :Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +21,17 @@ class MainActivity2 : AppCompatActivity() {
         aUserName = findViewById(R.id.text_view1)
         bUserPassword = findViewById(R.id.text_view2)
 
-        val username11 = intent.getStringExtra("username11")
-        val userpassword22 = intent.getStringExtra("userpassword22")
+        val username11 = intent.getStringExtra("Username")
+        val userpassword22 = intent.getStringExtra("UserPassword")
         aUserName.text = "$username11"
         bUserPassword.text = "$userpassword22"
+
+        var backbutton=findViewById<Button>(R.id.btn2)
+        backbutton.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+
+            Toast.makeText(this,"BACK Clicked", Toast.LENGTH_SHORT).show()
+        }
 
 
 
