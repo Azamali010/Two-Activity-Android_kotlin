07b5c618b81e45e97c45f29813f5051853f955c1
Azamali010 <azamali07711@gmail.com>
2023-07-31 15:01:10 +0530

    this is my fourth commit

--- app/src/main/java/com/example/twoactivity/MainActivity2.kt
+++ app/src/main/java/com/example/twoactivity/MainActivity2.kt
@@ -2,10 +2,17 @@ package com.example.twoactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var UserName : TextView
+    private lateinit var UserPassword :TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+
     }
 }
